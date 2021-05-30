3878d0a9b2b9ac614785f1a79ef9c6c61b833d1d
ironboy <thomas@nodehill.com>
2021-05-30 15:02:56 +0200

    correct kb calc

--- src/utilities/npmInfo.js
+++ src/utilities/npmInfo.js
@@ -3,7 +3,7 @@ export default async function (pname) {
     'https://bundlephobia.com/api/package-history?package=' + pname, { cache: "no-cache" })).json()).slice(-1)[0];
   pname += '@' + version;
   let size = await (await fetch('https://bundlephobia.com/api/size?package=' + pname, { cache: "no-cache" })).json();
-  let gzip = (Math.round(size.gzip / 100) / 10) + ' kB';
-  size = (Math.round(size.size / 100) / 10) + ' kB';
+  let gzip = (Math.round(size.gzip / 102.4) / 10) + ' kB';
+  size = (Math.round(size.size / 102.4) / 10) + ' kB';
   return { version, size, gzipped: gzip };
 }
