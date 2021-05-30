3bdd097ac5bd27ca492c8a57acd490f69dd6b601
ironboy <thomas@nodehill.com>
2021-05-30 15:38:56 +0200

    gzip serve

--- dist-backend/index.js
+++ dist-backend/index.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const compression = require('compression');
 const app = express();
+app.use(compression);
 app.use(express.static(path.join(__dirname, '../dist')));
 app.listen(4040, () => console.log('Listening on http://localhost:4040'));
