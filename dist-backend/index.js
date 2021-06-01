bd924bb0ec196de7c94726793253c44b260657bf
ironboy <thomas@nodehill.com>
2021-06-01 11:56:59 +0200

    ok with heroku

--- dist-backend/index.js
+++ dist-backend/index.js
@@ -2,4 +2,4 @@ const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.static(path.join(__dirname, '../dist')));
-app.listen(4040, () => console.log('Listening on port 4040'));
+app.listen(process.env.PORT || 4040, () => console.log('Listening...'));
